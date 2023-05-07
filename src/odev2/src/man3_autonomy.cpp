253bba504a0508ab7da160856454133edc7e72e6
onrdmr <onur.demir2@std.yildiz.edu.tr>
2023-05-07 17:21:23 +0300

    blue image mask added

--- src/odev2/src/man3_autonomy.cpp
+++ src/odev2/src/man3_autonomy.cpp
@@ -3,6 +3,8 @@
 #include "sensor_msgs/Image.h"
 
 #include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
+
 
 ros::Publisher cmd_vel_pub;
 geometry_msgs::Twist cmd_vel;
@@ -11,14 +13,42 @@ void cameraCallBack(const sensor_msgs::Image::ConstPtr& camera){
 	//ASAGIDA BULUNAN IF KOMUTU ORNEK OLARAK VERILMISTIR. SIZIN BURAYI DEGISTIRMENIZ BEKLENMEKTEDIR
 	//BURDAN SONRASINI DEGISTIR
 	
-	cv::Mat mat(camera->height, camera->width, CV_8UC3, const_cast<uchar *>(&camera->data[0]),
+	cv::Mat rgb_image(camera->height, camera->width, CV_8UC3, const_cast<uchar *>(&camera->data[0]),
     camera->step);
+	cv::Mat hsv_image, mask, masked_image, edges;
 
+	// gaussian blur
+    cv::Mat blurred_image;
+    cv::GaussianBlur(rgb_image, blurred_image, cv::Size(5, 5), 0.0, 0.0);
 
-	// cmd_vel.linear.x = 0.20;
-	
-	//BURDAN SONRASINA DOKUNMA
-	cv::imshow("OPENCV_WINDOW", mat);
+    // Display the original and blurred images side by side
+    cv::Mat side_by_side;
+    cv::hconcat(rgb_image, blurred_image, side_by_side);
+    cv::imshow("Original vs. Blurred", side_by_side);
+    // cv::waitKey(0);
+
+    
+    // // Convert RGB to HSV
+    cv::cvtColor(blurred_image, hsv_image, cv::COLOR_RGB2HSV);
+    // // Create mask for red color
+    cv::Scalar lower_red(110, 50, 50);
+    cv::Scalar upper_red(130, 255, 255);
+    cv::inRange(hsv_image, lower_red, upper_red, mask);
+    
+    // Apply mask to HSV image
+    // cv::bitwise_and(hsv_image, hsv_image, masked_image, mask);
+    
+    // // Convert HSV to RGB
+    // cv::cvtColor(masked_image, masked_image, cv::COLOR_HSV2RGB);
+    
+    // Detect edges in masked RGB image
+    // cv::Canny(masked_image, edges, 100, 200);
+    
+    // Display original RGB image, masked RGB image with edges, and wait for a key press
+    // cv::imshow("RGB Image", rgb_image);
+    cv::imshow("mask", mask);
+
+    // cv::imshow("Masked RGB Image with Edges", edges);
     cv::waitKey(3);
 
 	cmd_vel_pub.publish(cmd_vel);
