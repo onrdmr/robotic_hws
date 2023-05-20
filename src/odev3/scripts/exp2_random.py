a52f911330d9b6b9c2a838e0e4e4d533e6fdeef6
Melih Ocakcı <melihocaki@gmail.com>
2023-05-21 00:57:01 +0300

    debugging

--- src/odev3/scripts/exp2_random.py
+++ src/odev3/scripts/exp2_random.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
-import random, os, time, subprocess
+#!/usr/bin/env python3
+import random, os, time, subprocess, sys
 
 poses_x = [1.2, 0.0]
 poses_y = [1.2, 0.0, -1.2, -2.4, -3.6, -4.8, -6.0]
@@ -10,7 +10,13 @@ pose_y_i = random.randint(0, 6)
 pose_Y_i = random.randint(0, 3)
 pose_args = "-x " + str(poses_x[pose_x_i]) + " -y " + str(poses_y[pose_y_i]) + " -Y " + str(poses_Y[pose_Y_i])
 
-os.environ['RTG_POSE_ARGS'] = pose_args
-#print(os.environ['RTG_POSE_ARGS'])
+# os.environ['RTG_POSE_ARGS'] = pose_args
+# #print(os.environ['RTG_POSE_ARGS'])
+# time.sleep(3)
+# os.system("roslaunch p3at_description spawn_p3at.launch")
+
+sys.stderr.write("roslaunch p3at_description spawn_p3at.launch pose_args:=\"" + pose_args + "\"\n")
+sys.stderr.flush()
+
 time.sleep(3)
-os.system("roslaunch p3at_description spawn_p3at.launch")
+os.system("roslaunch p3at_description spawn_p3at.launch pose_args:=\"" + pose_args + "\"")
